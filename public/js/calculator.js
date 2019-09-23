32ce671ccc41760dbbcb46d2ec06a6ee1edf42b6
Grace Luo <grace.r.luo@gmail.com>
2019-09-22 22:10:11 -0700

    js

--- public/js/calculator.js
+++ public/js/calculator.js
@@ -1,3 +1,3 @@
 document.getElementById("weighted").addEventListener('click', function(){
-  console.log("you clicked me!"); //outputting to console (debugging)
+  window.alert("you clicked me!"); //outputting to console (debugging)
 });
