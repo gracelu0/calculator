55d91203767f2b717de52eca6ce6b9381cff4c44
Grace Luo <grace.r.luo@gmail.com>
2019-09-22 23:14:10 -0700

    mean

--- public/js/calculator.js
+++ public/js/calculator.js
@@ -1,17 +1,24 @@
 function calculatePercentage(num,denom){
+  console.log(num);
+  console.log(denom);
   var percentage = num/denom;
   roundedPercent = percentage.toFixed(2);
+  console.log(roundedPercent);
   return roundedPercent;
 }
 
 
-var mark1 = document.getElementById("a1mark").value;
-var total1 = document.getElementById("a1total").value;
-var percent1 = calculatePercentage(mark1, total1);
 
 
-
-document.getElementById("mean").addEventListener('click', function(){
+function calculateMean(){
+  var mark1 = document.getElementById("a1mark").value;
+  var total1 = document.getElementById("a1total").value;
+  var percent1 = calculatePercentage(mark1, total1);
   document.getElementById("p1").innerHTML = percent1;
   document.getElementById("result").innerHTML = percent1;
-});
+}
+
+// document.getElementById("mean").addEventListener('click', function(){
+//   document.getElementById("p1").innerHTML = percent1;
+//   document.getElementById("result").innerHTML = percent1;
+// });
