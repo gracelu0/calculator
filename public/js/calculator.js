081d8ce5c3856a0012db569fc1c5926249265c8f
Grace Luo <grace.r.luo@gmail.com>
2019-09-23 16:26:41 -0700

    mean

--- public/js/calculator.js
+++ public/js/calculator.js
@@ -7,16 +7,30 @@ function calculatePercentage(num,denom){
   return roundedPercent;
 }
 
-
-
-
+//Need to do error checking! (e.g. total=0, negatives)
 function calculateMean(){
-  var mark1 = document.getElementById("a1mark").value;
-  var total1 = document.getElementById("a1total").value;
-  var percent1 = calculatePercentage(mark1, total1);
-  document.getElementById("p1").innerHTML = percent1;
-  document.getElementById("result").innerHTML = percent1;
+  var allMarks = document.getElementsByName("mark");
+  var allTotals = document.getElementsByName("total");
+  var sumOfMeans = 0;
+  var mean = 0;
+  for (i = 0; i < allTotals.length;i++){
+    sumOfMeans+=calculatePercentage(allMarks[i],allTotals[i]);
+  }
+  mean = sumOfMeans/allTotals.length;
+  document.getElementById("result").innerHTML = mean;
+
 }
+
+// function calculateMean(){
+//   var mark1 = document.getElementById("a1mark").value;
+//   var total1 = document.getElementById("a1total").value;
+//   var percent1 = calculatePercentage(mark1, total1);
+//   document.getElementById("p1").innerHTML = percent1;
+//   document.getElementById("result").innerHTML = percent1;
+// }
+
+
+
 
 // document.getElementById("mean").addEventListener('click', function(){
 //   document.getElementById("p1").innerHTML = percent1;
