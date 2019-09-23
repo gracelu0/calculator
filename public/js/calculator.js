cb28ffb1c2abb87b41865a3d73c3544ba0bac026
Grace Luo <grace.r.luo@gmail.com>
2019-09-22 22:38:14 -0700

    js

--- public/js/calculator.js
+++ public/js/calculator.js
@@ -1,3 +1,16 @@
 document.getElementById("weighted").addEventListener('click', function(){
   window.alert("you clicked me!"); //outputting to console (debugging)
 });
+
+
+function calculatePercentage(num,denom){
+  var percentage = num/denom;
+  roundedPercent = percentage.toFixed(2);
+  return roundedPercent;
+}
+
+var mark1 = document.getElementById("a1mark").value;
+var total1 = document.getElementById("a1total").value;
+var percent1 = calculatePercentage(mark1, total1);
+
+document.getElementById("p1").innerHTML = percent1;
