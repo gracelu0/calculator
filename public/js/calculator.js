521d55d54d36e0bb6b9aa3f4a4702b3bac0c6247
Grace Luo <grace.r.luo@gmail.com>
2019-09-23 16:55:11 -0700

    weighted

--- public/js/calculator.js
+++ public/js/calculator.js
@@ -11,18 +11,44 @@ function calculatePercentage(num,denom){
 function calculateMean(){
   var allMarks = document.getElementsByName("mark");
   var allTotals = document.getElementsByName("total");
+  var numOfGrades = 0;
   var sumOfMeans = 0;
   var mean = 0;
   console.log(allTotals.length);
+
   for (i = 0; i < allTotals.length;i++){
     if (allMarks[i].value != '' && allTotals[i].value != ''){
       sumOfMeans+=parseFloat(calculatePercentage(allMarks[i].value,allTotals[i].value));
+      numOfGrades++;
       console.log(sumOfMeans);
     }
   }
-  mean = sumOfMeans/allTotals.length;
+  mean = sumOfMeans/numOfGrades;
   console.log(mean);
-  document.getElementById("result").innerHTML = "Mean of your grades is:"+ mean;
+  document.getElementById("result").innerHTML = "Mean of your grades is: "+ mean;
+
+}
+
+function calculateWeighted(){
+  var allMarks = document.getElementsByName("mark");
+  var allTotals = document.getElementsByName("total");
+  var allWeights = document.getElementsByName("weight");
+  var totalWeight = 0;
+  var sum = 0;
+  var weighted = 0;
+
+  for (i = 0; i < allTotals.length;i++){
+    if (allWeights[i].value > 0 && allMarks[i].value != '' && allTotals[i].value != ''){
+      grade = parseFloat(calculatePercentage(allMarks[i].value,allTotals[i].value));
+      sum+= parseFloat(grade*allWeights[i].value);
+      totalWeight+=parseFloat(allWeights[i].value);
+      console.log(sum);
+    }
+  }
+  weighted = sum/totalWeight;
+  console.log(weighted);
+  document.getElementById("result").innerHTML = "Your weighted grade is: "+ weighted;
+
 
 }
 
