98d2a68583b5e6e642d5ed935333fb18e93710f6
Grace Luo <grace.r.luo@gmail.com>
2019-09-23 21:20:53 -0700

    bgcolor

--- public/js/calculator.js
+++ public/js/calculator.js
@@ -51,19 +51,3 @@ function calculateWeighted(){
 
 
 }
-
-// function calculateMean(){
-//   var mark1 = document.getElementById("a1mark").value;
-//   var total1 = document.getElementById("a1total").value;
-//   var percent1 = calculatePercentage(mark1, total1);
-//   document.getElementById("p1").innerHTML = percent1;
-//   document.getElementById("result").innerHTML = percent1;
-// }
-
-
-
-
-// document.getElementById("mean").addEventListener('click', function(){
-//   document.getElementById("p1").innerHTML = percent1;
-//   document.getElementById("result").innerHTML = percent1;
-// });
